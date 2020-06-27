27112b6e33650ab1db9d2a3309e554909217898e
Mateus Queirós <mateuscqueiros1@gmail.com>
2020-06-27 13:51:44 -0300

    inputs com include do fields (+)

---
+++ instructors.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const data = require('./data.json')
+const { age, date } = require("./utils")
+
+// create
+
+exports.show = function(req, res) {
+    
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: Intl.DateTimeFormat("pt-BR").format(foundInstructor.create_at)
+    }
+
+    return res.render("instructors/show", { instructor })
+}
+
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        if (req.body[key] == "" ) {
+            return res.send("Please, fill all fields");
+        }
+    }
+
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        name,
+        avatar_url,
+        birth,
+        gender,
+        services,
+        created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error")
+
+        return res.redirect('/instructors')
+    })
+}
+
+exports.edit = function(req, res) {
+    
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+    
+    return res.render('instructors/edit', {instructor})
+}
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function(req, res) {
     return res.redirect("/instructors")
@@ -9,16 +10,14 @@ routes.get('/instructors', function(req, res) {
     return res.render("instructors/index")
 })
 
-routes.get('/members', function(req, res) {
-    return res.send("members")
-})
-
 routes.get("/instructors/create", function(req, res) {
     return res.render("instructors/create")
 })
 
-routes.post("/instructors", function(req, res) {
-    return res.send("Recebido.")
-})
+routes.get('/instructors/:id', instructors.show)
+
+routes.get('/instructors/:id/edit', instructors.edit)
+
+routes.post("/instructors", instructors.post)
 
 module.exports = routes
